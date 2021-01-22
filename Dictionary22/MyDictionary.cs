75ab57368a5a9552d4e648259cd7b8de9f1c2656
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-01-22 20:19:58 +0300

    Proje dosyası ekle.

---
+++ Dictionary22/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary22
+{
+    class MyDictionary<Id, Isim>
+    {
+        Id[] keys;
+        Isim[] values;
+
+        public MyDictionary()
+        {
+            keys = new Id[0];
+            values = new Isim[0];
+        }
+        public void Add(Id key, Isim value)
+        {
+            Id[] tempArrayKeys = keys;
+            Isim[] tempArrayValues = values;
+
+            keys = new Id[keys.Length + 1];
+            values = new Isim[values.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                values[i] = tempArrayValues[i];
+            }
+
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public Id[] Keys
+        {
+            get { return keys; }
+        }
+
+        public Isim[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ Dictionary22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dictionary22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("1579", "Elif Duru");
+            myDictionary.Add("4565", "Kaan Alp");
+            myDictionary.Add("6879", "Selcen Akarsu");
+
+
+
+            foreach (var IdNo in myDictionary.Keys)
+            {
+                Console.WriteLine(IdNo + " : Aranıyor... ");
+
+            }
+
+            foreach (var Isim in myDictionary.Values)
+            {
+                Console.WriteLine("Eşleşen Isim: " + Isim);
+            }
+
+        }
+    }
+}
